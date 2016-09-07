0274d43ddcb6201ae5d4e9ca5d612f903b8f7d02
evry.collin@gmail.com <evry.collin@gmail.com>
2016-09-07 08:52:31 +0200

    ml

--- index.js
+++ index.js
@@ -2,13 +2,43 @@
 
 var MyPlugin = function(webdriverInstance, options) {
     options = options || {};
+    
+    this.currentTest=null;
 
     if(!webdriverInstance) {
         throw new Error('A WebdriverIO instance is needed to initialise MyPlugin');
     }
     
-    console.log('!!!!!!!!!!!!!!!!','MyPlugin','init');
+    //console.log('!!!!!!!!!!!!!!!!','MyPlugin','init');
     
+    webdriverInstance.addCommand('hello', function() {
+    
+      console.log('!!!!!!!!!!!!!!!!!!!!!!!', 'HELLO');
+    
+    } );
+    
+    
+    process.on('test:start', function(test) {
+      browser.cid = test.cid;
+    });    
+/*
+{
+	type: 'test: start',
+	title: 'home_check_title#fr_FR',
+	parent: 'com.ldn.home.fr_FR',
+	pending: false,
+	file: 'D: \\orchestra\\listedenaissance\\test\\specs\\home.js',
+	cid: '0a',
+	specs: ['D: \\orchestra\\listedenaissance\\test\\specs\\home.js'],
+	runner: {
+		'0a': {
+			maxInstances: 1,
+			browserName: 'chrome'
+		}
+	}
+}
+*/      
+
 }
 
 
